bdb26ed233e06cd42f81e08cafca0882b0e43e24
Bilal Taherkheli <bilal76@gmail.com>
2022-12-11 21:23:00 +0100

    D07 complete

---
+++ src/D07/Filex.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace aoc.D07
+{
+  public class Filex
+  {
+    private string _name;
+    private int _size;
+    private Folder _folder;
+
+    public Filex(string name, int size, Folder folder)
+    {
+      _name = name;
+      _size = size;
+      _folder = folder;
+    }
+
+    public string Name
+    {
+      get => _name;
+      set => _name = value;
+    }
+
+    public int Size
+    {
+      get => _size;
+      set => _size = value;
+    }
+
+    public Folder Folder
+    {
+      get => _folder;
+      set => _folder = value;
+    }
+  }
+}
---
+++ src/D07/Folder.cs
@@ -0,0 +1,68 @@
+﻿namespace aoc.D07
+{
+  public class Folder
+  {
+    private string _name;
+    private int _size;
+    private Folder? _parent;
+    readonly List<Folder> _folders;
+    readonly List<Filex> _files;
+
+    public Folder(string name, Folder? parent)
+    {
+      _name = name;
+      _parent = parent;
+      _folders = new List<Folder>();
+      _files = new List<Filex>();
+    }
+
+    public string Name
+    {
+      get => _name;
+      set => _name = value;
+    }
+
+    public int Size
+    {
+      get => ComputeSize();
+    }
+
+    public Folder? Parent
+    {
+      get => _parent;
+      set => _parent = value;
+    }
+
+    public List<Folder> Folders
+    {
+      get => _folders;
+    }
+
+    public List<Filex> Files
+    {
+      get => _files;
+    }
+
+    public void AddChildFolder(Folder folder)
+    {
+      _folders.Add(folder);
+    }
+
+    public void AddFile(Filex file)
+    {
+      _files.Add(file);
+    }
+
+    private int ComputeSize()
+    {
+      int fileSum = 0, dirSum = 0;
+      foreach (var file in _files)      
+        fileSum += file.Size;      
+
+      foreach (var folder in _folders)
+        dirSum += folder.Size;
+
+      return fileSum + dirSum;
+    }
+  }
+}
