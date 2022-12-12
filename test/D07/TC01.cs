edd341b5e2ac7dd9b6a3875bcb3ae9b2f2db837e
Bilal Taherkheli <bilal76@gmail.com>
2022-12-12 19:36:04 +0100

    D07 complete

--- test/D07/TC01.cs
+++ test/D07/TC01.cs
@@ -1,31 +1,28 @@
-//using aoc.D05;
-//using aoc.D07;
-//using Xunit;
+using aoc.D07;
+using Xunit;
 
-//namespace aoc.test.D07
-//{
-//  public class TC01
-//  {
-//    [Fact]
-//    public void InitialI()
-//    {
-//      //var expected = "CMZ";
-//      var shell = new Shell(@"./D07/initial.txt");
-//      var expected = shell.PartI();
+namespace aoc.test.D07
+{
+  public class TC01
+  {
+    [Theory]
+    [InlineData("initial.txt", 95437)]
+    [InlineData("input.txt", 1454188)]
+    public void PartI(string fileName , int expected) 
+    {
+      var shell = new Shell(@"./D07/" + fileName);
+      var actual = shell.PartI();
+      Assert.Equal(expected, actual);
+    }
 
-//      //var actual = p.GetTopCrates();
-//      //Assert.Equal(expected, actual);
-//    }
-
-//    //[Theory]
-//    //[InlineData(4, 1361)]
-//    //[InlineData(14, 3263)]
-//    //public void PartII(int windowSize, int expected)
-//    //{
-//    //  var text = File.ReadAllText(@"./D07/input.txt");
-//    //  var d = new Device();
-//    //  var actual = d.PacketStartMarker(text, windowSize);
-//    //  Assert.Equal(expected, actual);
-//    //}
-//  }
-//}
+    [Theory]
+    [InlineData("initial.txt", 24933642)]
+    [InlineData("input.txt", 4183246)]
+    public void PartII(string fileName, int expected)
+    {
+      var shell = new Shell(@"./D07/" + fileName);
+      var actual = shell.PartII();
+      Assert.Equal(expected, actual);
+    }
+  }
+}
